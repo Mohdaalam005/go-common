76df65993a47d6566751914f1402fb5b1e843631
mohd aalam <skanawade@greenlighthealth.com>
2023-08-01 16:29:28 +0530

    updated the databse

--- database/database.go
+++ database/database.go
@@ -3,10 +3,16 @@ package database
 import (
 	"database/sql"
 	"github.com/pkg/errors"
+	"time"
 
 	"fmt"
 	_ "github.com/lib/pq"
 	log "github.com/sirupsen/logrus"
+)
+
+const (
+	defaultMaxIdleConnections = 5
+	defaultMaxOpenConnections = 50
 )
 
 var db *sql.DB
@@ -16,16 +22,18 @@ type DbConnection struct {
 }
 
 type DbConfig struct {
-	Host   string
-	Port   int
-	User   string
-	Pass   string
-	DbName string
+	Host            string
+	Port            int
+	User            string
+	Pass            string
+	DbName          string
+	MaxIdleConn     int
+	MaxOpenConn     int
+	MaxLifetimeMins int
 }
 
 func InitDatabase(config DbConfig) (DbConnection, error) {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		config.Host, config.Port, config.User, config.Pass, config.DbName)
 
 	connection := DbConnection{}
@@ -37,6 +45,26 @@ func InitDatabase(config DbConfig) (DbConnection, error) {
 			return DbConnection{}, errors.Wrap(err, "while connecting to database")
 		}
 		connection.Conn = pdb
+		if config.MaxIdleConn == 0 {
+			log.Debugf("InitDatabase:: setting SetMaxIdleConns to %d", defaultMaxIdleConnections)
+			connection.Conn.SetMaxIdleConns(defaultMaxIdleConnections)
+		} else {
+			log.Debugf("InitDatabase:: setting SetMaxIdleConns to %d", config.MaxIdleConn)
+			connection.Conn.SetMaxIdleConns(config.MaxIdleConn)
+		}
+
+		if config.MaxOpenConn == 0 {
+			log.Debugf("InitDatabase:: setting SetMaxOpenConns to %d", defaultMaxOpenConnections)
+			connection.Conn.SetMaxOpenConns(defaultMaxOpenConnections)
+		} else {
+			log.Debugf("InitDatabase:: setting SetMaxOpenConns to %d", config.MaxOpenConn)
+			connection.Conn.SetMaxOpenConns(config.MaxOpenConn)
+		}
+
+		if config.MaxLifetimeMins != 0 {
+			log.Debugf("InitDatabase:: setting ConnMaxLifetime to %d mins", config.MaxLifetimeMins)
+			connection.Conn.SetConnMaxLifetime(time.Minute * time.Duration(config.MaxLifetimeMins))
+		}
 	}
 	return connection, nil
 }
